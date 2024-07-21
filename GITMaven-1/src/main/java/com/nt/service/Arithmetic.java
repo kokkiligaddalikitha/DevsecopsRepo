2f56c65331847ee7fb13efd9d983cb81c941201a
kokkiligaddalikitha <kokkiligaddalikitha@gmail.com>
2024-07-21 10:19:19 +0530

    dev changes to sum(-,-) method

--- GITMaven-1/src/main/java/com/nt/service/Arithmetic.java
+++ GITMaven-1/src/main/java/com/nt/service/Arithmetic.java
@@ -2,7 +2,7 @@ package com.nt.service;
 
 public class Arithmetic {
 	public int sum(int x,int y) {
-		System.out.println("Arithmetic.sum() (begin)");
+		System.out.println("****Arithmetic.sum() (begin)****");
 		System.out.println("***************");
 		return x+y;
 	}
