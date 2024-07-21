e5c88e03d888a3539192ae665d540490af090312
kokkiligaddalikitha <kokkiligaddalikitha@gmail.com>
2024-07-21 10:32:42 +0530

    deleting some code

--- GITMaven-1/src/main/java/com/nt/service/Arithmetic.java
+++ GITMaven-1/src/main/java/com/nt/service/Arithmetic.java
@@ -2,13 +2,6 @@ package com.nt.service;
 
 public class Arithmetic {
 	public int sum(int x,int y) {
-
-		System.out.println("----Arithmetic.sum() (start)----");
-		System.out.println("-------");
-
-		System.out.println("****Arithmetic.sum() (begin)****");
-		System.out.println("***************");
-
 		return x+y;
 	}
 
