aca5ed441afee1a334bef742ed9298bc6dcdecee
kokkiligaddalikitha <kokkiligaddalikitha@gmail.com>
2024-07-21 11:19:21 +0530

    test changes to (-240)

--- GITMaven-1/src/test/java/com/nt/test/AppTest.java
+++ GITMaven-1/src/test/java/com/nt/test/AppTest.java
@@ -22,8 +22,8 @@ public class AppTest
 	public void testWithNegatives()
 	{
 		Arithmetic ar=new Arithmetic();
-		int expected=-40;
-		int actual=ar.sum(-20, -20);
+		int expected=-240;
+		int actual=ar.sum(-200, -20); //-20  -20
 		assertEquals(expected, actual);
 	}
 
