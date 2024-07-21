8d2838f7dca6489d5db2245e81a1cd6064600714
kokkiligaddalikitha <kokkiligaddalikitha@gmail.com>
2024-07-21 10:45:17 +0530

    sum

--- GITMaven-1/src/main/java/com/nt/service/Arithmetic.java
+++ GITMaven-1/src/main/java/com/nt/service/Arithmetic.java
@@ -2,7 +2,8 @@ package com.nt.service;
 
 public class Arithmetic {
 	public int sum(int x,int y) {
-		System.out.println("Arithmetic.sum() begin");
+		System.out.println("Arithmetic.sum() begin ");
+		System.out.println();
 		return x+y;
 	}
 
